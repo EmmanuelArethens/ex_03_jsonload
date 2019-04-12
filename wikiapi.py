c01d5cfc05569141a7d0626fe32ab7282dfd2a36
Arethens <emmanuel.arethens1@gmail.com>
2019-04-12 15:19:52 +0200

    trying to make a request to wikipedia's api

---
+++ wikiapi.py
@@ -0,0 +1,22 @@
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+TITLE = 'Wikimedia Foundation'
+
+PARAMS = {
+    'action':"query",
+    'prop':"coordinates",
+    'titles': TITLE,
+    'format':"json"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+PAGES = DATA['query']['pages']
+
+for k, v in PAGES.items():
+    print("Latitute: " + str(v['coordinates'][0]['lat']))
+    print("Longitude: " + str(v['coordinates'][0]['lon']))
