c39262e8ff7d6a92fd0748a1582569c85d401b57
Arethens <emmanuel.arethens1@gmail.com>
2019-04-15 17:12:10 +0200

    testing to grab commits

--- githubapi.py
+++ githubapi.py
@@ -2,19 +2,36 @@ import requests
 
 username = input("github username :")
 
-class GetUser:
+class GetRepos:
     def __init__(self, url = "https://api.github.com/users/" + username + "/repos"):
         self.url = url
 
-    def get(self):
+    def getrep(self):
         S = requests.Session()
         R = S.get(self.url)
         DATA = R.json()
         return DATA
 
-list_repo = GetUser().get()
+list_repo = GetRepos().getrep()
 repo = []
 
 for i in list_repo:
     repo.append(i["name"])
 print(repo)
+
+class GetCommit:
+    def __init__(self, url = "https://api.github.com/users/" + username + "/ex_03_jsonload/commits"):
+        self.url = url
+
+    def getcom(self):
+    	S = requests.Session()
+    	R = S.get(self.url)
+    	DATA = R.json()
+    	return DATA
+
+list_commit = GetCommit().getcom()
+com = []
+
+for c in list_commit:
+	com.append(c["commit"])
+print(com)
